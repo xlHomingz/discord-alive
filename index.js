67f802f12dd3014d4ff4b8f5628875ef1c7f17c2
xlHomingz <76704738+xlHomingz@users.noreply.github.com>
2024-03-10 19:24:36 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const keep_alive = require('./keep_alive.js')
 
 // Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
-
+client.user.setActivity(`${config.prefix}gen │ ${config.servername}`, { type: "STREAMING", url: "https://twitch.tv/wtcn" });
 bot.on("error", (err) => {
   console.error(err); // or your preferred logger
 });
